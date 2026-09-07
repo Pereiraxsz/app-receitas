ffd9ba3aac2e04ac62975c38f31551287979974c
samuel <samuelsz1005@gmail.com>
2026-09-07 00:33:06 -0300

    Organiza estrutura base da main

--- lib/main.dart
+++ lib/main.dart
@@ -1,20 +1,23 @@
 import 'package:flutter/material.dart';
 
-import 'screens/lista_receitas_screen.dart';
-
 void main() {
-  runApp(const MeuApp());
+  runApp(const AppReceitas());
 }
 
-class MeuApp extends StatelessWidget {
-  const MeuApp({super.key});
+class AppReceitas extends StatelessWidget {
+  const AppReceitas({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      home: const ListaReceitasScreen(
-        categoria: 'Massas',
+      home: Scaffold(
+        appBar: AppBar(
+          title: const Text('App de Receitas'),
+        ),
+        body: const Center(
+          child: Text('Projeto em desenvolvimento'),
+        ),
       ),
     );
   }
